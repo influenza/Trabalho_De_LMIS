121bf53cf4bf6e97a626187f5fe5e3b04d16e16a
influenza <118693099+influenza@users.noreply.github.com>
2023-06-14 20:23:26 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -8,22 +8,6 @@ import { BrowserRouter, Route, Link } from 'react-router-dom';
  function App() {  
   const [postado, setpostado] = useState(false);
   const [data, setData] = useState("");
-
-  useEffect(() => {
-    console.log(postado);
-    if (postado === false) {
-      setpostado(true);
-    } else {
-      axios.post('http://127.0.0.1:5000/my-datas', {
-        plusone: 1
-      }).then(response => {
-        console.log(response.data);
-        setData(response.data)
-      }).catch(error => {
-        console.log(error);
-      });
-    }
-  }, [postado]);
   return (
     <>
       <div>
